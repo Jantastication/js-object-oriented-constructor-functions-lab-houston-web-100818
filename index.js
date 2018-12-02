2827c05fe79e714f223353c832fde4932b01a73a
github username <github email address>
2018-12-02 12:29:37 -0600

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+// let lime = new Scooter('2018', 'black', 'lime')
+
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+// let bruh = new Driver('bruh', 34, 12)
+
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
